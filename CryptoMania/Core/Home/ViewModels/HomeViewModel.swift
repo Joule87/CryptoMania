fe5208f2a232f2d313ae288023d234594edae5d8
julio.collado <julio.collado@overactive.com>
2021-11-02 22:29:55 -0300

    Requested all coins

---
+++ CryptoMania/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewModel.swift
+//  CryptoMania
+//
+//  Created by Julio Collado on 2/11/21.
+//
+
+import Foundation
+import Combine
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolio: [CoinModel] = []
+    
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] receivedCoins in
+                guard let self = self else { return }
+                self.allCoins = receivedCoins
+            }
+            .store(in: &cancellables)
+    }
+}
