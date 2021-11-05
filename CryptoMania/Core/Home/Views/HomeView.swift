da6767fe3d0d33716ac420d08dff67c6ef4e9993
julio.collado <julio.collado@overactive.com>
2021-11-05 09:15:20 -0300

    Created SearchBarView Component

--- CryptoMania/Core/Home/Views/HomeView.swift
+++ CryptoMania/Core/Home/Views/HomeView.swift
@@ -20,6 +20,7 @@ struct HomeView: View {
             
             VStack {
                 homeHeader
+                SearchBarView(searchText: $viewModel.searchText)
                 columnTitles
                 
                 if !showPortfolio {
